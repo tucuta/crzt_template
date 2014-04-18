3e7a4569599eac1baccf788de5f77cf845d21a44
Rosen Zahariev <rosen4o@gmail.com>
2014-04-18 08:34:49 +0200

    Added new GA tracking code

--- lib/crzt.php
+++ lib/crzt.php
@@ -131,7 +131,7 @@ class CRZT{
 	    $document->addStyleSheet($this->API->baseurl."/templates/".$this->API->template."/css/template.css");	    
 
 	    if($this->getParam('google_analytics', '')){
-		$script =  "<script type=\"text/javascript\">var _gaq = _gaq || []; _gaq.push(['_setAccount', '".$this->getParam('google_analytics', '')."']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>";
+		$script =  '<script type="text/javascript">var _gaq = _gaq || []; _gaq.push([\'_setAccount\', \'' .$this->getParam('google_analytics', ''). '\']); _gaq.push([\'_trackPageview\']);(function() { var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s); })();</script>';
 		$document->addScriptDeclaration($script, $type);
 	    }
 	    if($this->getParam('google_webmastertools', '')){
